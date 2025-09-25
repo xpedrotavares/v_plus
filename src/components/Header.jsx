8e6dab159b36ebe96d1a3980d881eb848b762bd5
Pedro Tavares <xpedrotavaresdev@gmail.com>
2025-09-25 15:43:16 +0000

    fix relative path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="fixed top-0 left-0 right-0 bg-black/50 backdrop-blur-lg p-4 z-50">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <img src="../assets/logo.png" alt="Vaccine+" className="w-44" />
+        <img src="./assets/logo.png" alt="Vaccine+" className="w-44" />
         {/* Usando o novo componente ShinyButton */}
         <a href="https://wa.me/15556654247?text=Ol%C3%A1%2C%20quero%20iniciar%20minha%20avalia%C3%A7%C3%A3o%20vacinal!" className='border rounded-[15px]  border-[#353535] bg-[#111]'>
             
