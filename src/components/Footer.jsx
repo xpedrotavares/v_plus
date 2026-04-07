7ccb5d4e981d1d407deeb1b7bef022470ff18a6c
Pedro Tavares <xpedrotavaresdev@gmail.com>
2026-04-07 20:50:32 +0000

    Add footer info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
     <footer className="text-center p-6 bg-black">
-      <p className="text-gray-500">&copy; 2025 Vaccine+. Todos os direitos reservados.</p>
+      <p className="text-gray-500">
+        Vaccine+. Todos os direitos reservados.
+      </p>
+      <p className="text-gray-500">
+        Vaccine+ é um produto Health Clinic SP (CNPJ: 34.412.094/0001-28)
+      </p>
     </footer>
   );
 };
