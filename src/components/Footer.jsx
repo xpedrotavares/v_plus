7ff23cbe60fe1f8bbfdb40512787dfa0eff7f5ae
Pedro Tavares <xpedrotavaresdev@gmail.com>
2026-04-09 18:34:08 +0000

    fix footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,9 +12,9 @@ const Footer = ({ onOpenPrivacy }) => {
           Política de Privacidade
         </button>
       </div>
-      <p className="text-gray-500">Vaccine+. Todos os direitos reservados.</p>
+      <p className="text-gray-500">VaccineMais. Todos os direitos reservados.</p>
       <p className="text-gray-500">
-        Vaccine+ é um produto Health Clinic SP (CNPJ: 34.412.094/0001-28)
+        VaccineMais é um produto Health Clinic SP (CNPJ: 34.412.094/0001-28)
       </p>
     </footer>
   );
